5153335023c564abf2de2a1a7adeb043e5d30bf3
futami <>
2020-08-25 21:58:22 +0900

    [Add] プログレスバー追加

--- Diary/Diary/ViewController/WebView/WebViewController.swift
+++ Diary/Diary/ViewController/WebView/WebViewController.swift
@@ -12,6 +12,7 @@ import WebKit
 class WebViewController: UIViewController {
     
     var wkWebView = WKWebView()
+    var progressView = UIProgressView()
     var urlString: String?
     
     deinit {
@@ -22,9 +23,32 @@ class WebViewController: UIViewController {
         super.viewWillAppear(animated)
         self.setupNavigation()
         self.setupWebView()
+        self.setupProgressView()
         self.showWebView(self.urlString)
     }
     
+    override func observeValue(forKeyPath keyPath: String?,
+                               of object: Any?,
+                               change: [NSKeyValueChangeKey : Any]?,
+                               context: UnsafeMutableRawPointer?) {
+        if (keyPath == "estimatedProgress") {
+            self.progressView.alpha = 1.0
+            // estimatedProgressが変更されたときにプログレスバーの値を変更
+            self.progressView.setProgress(Float(self.wkWebView.estimatedProgress), animated: true)
+            
+            // estimatedProgressが1.0になったらアニメーションを使って非表示にしアニメーション完了時0.0をセットする
+            if (self.wkWebView.estimatedProgress >= 1.0) {
+                UIView.animate(withDuration: 0.3, delay: 0.3, options: [.curveEaseOut], animations: { [weak self] in
+                    self?.progressView.alpha = .zero
+                    }, completion: { (finished : Bool) in
+                        self.progressView.setProgress(0.0, animated: false)
+                })
+            }
+        }
+    }
+}
+
+private extension WebViewController {
     func setupNavigation() {
         self.navigationController?.navigationBar.barTintColor = .white
         self.navigationController?.navigationBar.tintColor = .black
@@ -40,13 +64,25 @@ class WebViewController: UIViewController {
     
     // webViewサイズ設定
     func setupWebView() {
-        let rect = CGRect(x: .zero,
-                          y: .zero,
+        let rect = CGRect(x: .zero, y: .zero,
                           width: view.frame.width,
                           height: view.frame.height)
         
         let webConfiguration = WKWebViewConfiguration()
         self.wkWebView = WKWebView(frame: rect, configuration: webConfiguration)
+    }
+    
+    func setupProgressView() {
+        self.progressView = UIProgressView(frame: CGRect(x: 0.0,
+                                                         y: (self.navigationController?.navigationBar.frame.size.height ?? .zero) - 2,
+                                                         width: self.view.frame.size.width,
+                                                         height: 2))
+        self.progressView.progressViewStyle = .bar
+        self.navigationController?.navigationBar.addSubview(self.progressView)
+        
+        // KVO 監視
+        self.wkWebView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+        self.wkWebView.addObserver(self, forKeyPath: "loading", options: .new, context: nil)
     }
     
     @objc func closeButtonAction() {
